8a9fa637ae4d93de4701f2a20ec52a8a6fbfb17e
silas <jose.silasjr.bb@gmail.com>
2018-08-09 16:15:45 -0400

    implementacao de validacao da autenticacao

--- auth.js
+++ auth.js
@@ -1,7 +1,10 @@
 module.exports = (app) => {
     return api = {
+        isValid (credentials) {
+            return true
+        },
         authentication (credentials) {
-            if (isValid(credentials)) {
+            if (this.isValid(credentials)) {
                 console.log(credentials.username);
             }
         }
